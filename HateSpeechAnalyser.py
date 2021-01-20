2f61678545cb1e2a86ceb7f84bd015773a0f2984
Holger Johansson <holger.johansson@hotmail.se>
2021-01-20 14:40:48 +0100

    Just tried the files out in PyCharm instead of Jupyter. Removed some comments.

--- HateSpeechAnalyser.py
+++ HateSpeechAnalyser.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import hatesonar
 import pandas as pd
 import json
--- RedditDataDL.py
+++ RedditDataDL.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[15]:
-
-
 import requests
 import json
 import os
