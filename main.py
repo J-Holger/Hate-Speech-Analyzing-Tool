ac393be2d365d04fb54b53e35ff69c12aad89e3a
Holger Johansson <holger.johansson@hotmail.se>
2021-02-10 17:45:53 +0100

    Added a new regex.

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import RedditDataDL as rddl
 import pandas as pd
 
 # Initialize RedditDataDL with parameters according to https://github.com/pushshift/api
-d = rddl.RedditDataDL(endpoint='comment', before="1609459200",
-                               after="1577836800",subreddit='FemaleDatingStrategy')
+#d = rddl.RedditDataDL(endpoint='comment', before="1609459200",
+#                               after="1577836800",subreddit='FemaleDatingStrategy')
 
 # Download the Reddit data. Default is 500 posts per call.
-data_dir, metadata_dir = d.get_data()
-
+#data_dir, metadata_dir = d.get_data()
+data_dir = 'C:/Users/holge/my_projects/HateSpeechAnalyzingTool/data/TheRedPill_comment_1577836800_1609459200/data/'
+metadata_dir = 'C:/Users/holge/my_projects/HateSpeechAnalyzingTool/data/TheRedPill_comment_1577836800_1609459200/metadata/'
 
 
 # Initialize HateSpeechAnalyzer.
@@ -17,10 +18,9 @@ a = hsa.HateSpeechAnalyzer()
 # Load the Reddit data that was downloaded into the HateSpeechAnalyzer.
 a.load_json(data_dir, metadata_dir)
 
-# Check that the data looks good
-print( a.data.head() )
-print( a.metadata.head() )
-print(a.data.shape)
+# Print the shapes of the data
+print('Data has {} rows and {} columns'.format(a.data.shape[0], a.data.shape[1]))
+
 
 # Define a list of comments that you want removed.
 comment_list = {'[removed]', '[deleted]', '_URL_'}
@@ -33,10 +33,12 @@ regex_input = {r'@\w+' : '@USER ', # Replace a mention by @ with @User
               r'&gt;\s' : '', # Removes &gt; with a space after
               r'&gt;' : '', # Removes &gt; without a space after
               r'&amp;x200B;' : '', # Removes &amp;x200B;
-              }
+              r'^\s+|\s+$' : '', # Remove leading and trailing spaces
+              r'^\W+' : ''}
 
 # Clean data in the given column and store the record.
-removed_items = a.clean_data('body', disallowed=comment_list, regex=regex_input)
+a.apply_regex('body', regex_input)
+removed_items = a.clean_data('body', disallowed=comment_list)
 
 # Make a DataFrame of the record so it's easy to handle.
 removed_items = pd.DataFrame(data=removed_items)
